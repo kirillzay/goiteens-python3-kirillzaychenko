71d33921443f77bd15b3eb532343349cbe95d1ad
John Doe <kirilozaichenko@gmail.com>
2021-03-06 13:36:28 +0200

    home work 1

---
+++ home work 1.py
@@ -0,0 +1,22 @@
+
+blocks = 2
+seconds = 10
+all_seconds = blocks * seconds
+print("1 задача.",all_seconds,"блоків проролітає бджола в майнкрафті за ",seconds," секунд") #задача номер 1 
+
+Minecrafter1999 = 2000
+DoctorHto = 50
+riz = Minecrafter1999 - DoctorHto
+print("2 задача. на",riz,"підписників у майнкрафтера1999 більше")
+
+DoctorHto_all_views = 1000
+_5_kopyiok = 50
+DoctorHto_money = _5_kopyiok * DoctorHto_all_views
+print("3 задача. всього",DoctorHto_money,"копійок заробив докторхто")
+
+katya_years = 10
+paint = 15
+painting_in_1_year = 5
+years = paint - katya_years
+painting = years * painting_in_1_year
+print("4 задача. ",painting,"картин намалює Катя до 15 років")

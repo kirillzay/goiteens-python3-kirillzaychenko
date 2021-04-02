c54f55f7151301e57884c12b4bb912fbb657dfc1
John Doe <kirilozaichenko@gmail.com>
2021-04-02 18:53:04 +0300

    homework5

---
+++ homework5.py
@@ -0,0 +1,16 @@
+#1 задача
+group = [ "Гітарист", "Барабанщик", "Піаніст", "Вокаліст"]
+for i in group:
+    print(i)
+#2 задача
+musition_money = 100 * 4
+all_group_money =musition_money * 12
+print(all_group_money)
+#3 задача
+python_on_gitar = [ "Пайтон і джунглі", "Прінт та вивід на консоль моїх емоцій", "Інпут свій настрій"]
+for i in range(0,3):
+    print(i + 1 ,". ",python_on_gitar[i])
+#4 задача
+likes = 0
+for i in range(1,6):
+    print(i * 123 ,"загалом лайків після години ", i)

5ec9780f22601d4850319d287dd6e1b6537c2c88
John Doe <kirilozaichenko@gmail.com>
2021-04-24 20:49:01 +0300

    homework 9

--- calendar.py
+++ calendar.py
@@ -1,18 +1,47 @@
-print("Привіт це мій календар")
-print("Виберіть функцію")
-print("1. Записати нову подію")
-print("2. Прочитати всі події")
-print("3. Вийти")
-selected_function = input("введіть функцію яку ви вибрали: ")
+#functions
+def menu():
+    print("Привіт це мій календар")
+    print("Виберіть функцію")
+    print("1. Записати нову подію")
+    print("2. Прочитати всі події")
+    print("3. Вийти")
 
-if selected_function == "1":
-    neu_date = input("Введіть дату: ")
+    return input("введіть функцію яку ви вибрали: ")
+
+
+def addevent():
+    neu_date = input("Введіть подію в форматі Пріоритет/Дата (yyyy.mm.dd)/Назва: ")
     calendar = open("calendar.txt", "a")
     calendar.write("\n" + neu_date)
     calendar.close()
+
+def sortedbypriority(all_events):
+    return sorted(all_events, key= lambda f : f[0])
+
+def sortedbydate(all_events):
+   return sorted(all_events, key= lambda f : f[1])
+#main program
+
+
+selected_function = menu()
+all_events = list()
+if selected_function == "1":
+    addevent()
 if selected_function == "2":
+    #calendar = neu_date.sorted(open("calendar.txt", "r"))
+    #print(calendar.read())
     calendar = open("calendar.txt", "r")
-    print(calendar.read())
+    lst=calendar.readlines()
+    print(lst)
+    for ln in lst:
+        all_events.append(ln.split("/"))
+    print(all_events)
+    sorted_by_priority = sortedbypriority(all_events)
+    print("Сортування за пріоритетом: ", sorted_by_priority)
+    sorted_by_date = sortedbydate(all_events)
+    print("сортування за датою: ", sorted_by_date)
+
+    
 if selected_function == "3":
     print("Допобачення!!!")
 
